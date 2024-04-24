f10004a5fc0a6dd595fb89ee519d60e112219bbe
zambrix20 <companero20112011@hotmail.com>
2024-04-24 17:15:25 -0600

    Initial project🎉

---
+++ src/app/home/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function HomePage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-950">
+            <header className="flex items-center justify-center">
+                <h1 className="text-4xl font-bold text-center text-white">
+                    ¡Bienvenido a Deporte Local!
+                </h1>
+            </header>
+        </main>
+    )
+}
